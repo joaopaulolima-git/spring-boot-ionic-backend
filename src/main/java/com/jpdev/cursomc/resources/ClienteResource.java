3421a5d909431600332b1f5099c7b20fd018def9
João Paulo <jplopapa.git@gmail.com>
2022-07-30 17:23:42 -0300

    Endpoint /clientes/{id}

--- src/main/java/com/jpdev/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jpdev/cursomc/resources/ClienteResource.java
@@ -7,21 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jpdev.cursomc.domain.Categoria;
-import com.jpdev.cursomc.services.CategoriaService;
+import com.jpdev.cursomc.domain.Cliente;
+import com.jpdev.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value = "/categorias")
-public class CategoriaResource {
+@RequestMapping(value = "clientes")
+public class ClienteResource {
 	
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
-	@RequestMapping(value= "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Categoria> find(@PathVariable Integer id) {		
-		Categoria obj = service.find(id);
-		return ResponseEntity.ok().body(obj);		
-		
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Cliente> find(@PathVariable Integer id){
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/jpdev/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.jpdev.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jpdev.cursomc.domain.Cliente;
+import com.jpdev.cursomc.repositories.ClienteRepository;
+import com.jpdev.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id" + id + ", Tipo " + Cliente.class.getName()));
+	}
+
+}
