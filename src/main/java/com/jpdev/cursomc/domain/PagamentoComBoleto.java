fe5ad793898c0506945256c3b177c7b244a12947
João Paulo <jplopapa.git@gmail.com>
2022-08-02 11:02:48 -0300

    Endpoint /pedidos/{id}disponibilizado

--- src/main/java/com/jpdev/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/jpdev/cursomc/domain/PagamentoComBoleto.java
@@ -4,13 +4,16 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jpdev.cursomc.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern = "dd/MM/yyy")
 	private Date dataVencimento;
+	@JsonFormat(pattern = "dd/MM/yyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
