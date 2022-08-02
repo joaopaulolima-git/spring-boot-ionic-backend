453eb06829091e21bad2b723ebd39bd549702d81
João Paulo <jplopapa.git@gmail.com>
2022-08-02 16:33:46 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/jpdev/cursomc/services/CategoriaService.java
+++ src/main/java/com/jpdev/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.jpdev.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,9 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityExcepition("Não é possivel deletar uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
