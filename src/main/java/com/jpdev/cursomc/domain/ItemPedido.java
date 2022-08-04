71aec346d9f6393fa2d1e64a3f280541c2e78d3d
João Paulo <jplopapa.git@gmail.com>
2022-08-04 16:54:36 -0300

    Subtotal e Total do Pedido

--- src/main/java/com/jpdev/cursomc/domain/ItemPedido.java
+++ src/main/java/com/jpdev/cursomc/domain/ItemPedido.java
@@ -18,18 +18,22 @@ public class ItemPedido implements Serializable {
 	
 	private Double desconto;
 	private Integer quantidade;
-	private Double preço;
+	private Double preco;
 
 	public ItemPedido() {
 	}
 
-	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preço) {
+	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
 		id.setPedido(pedido);
 		id.setProduto(produto);
 		this.desconto = desconto;
 		this.quantidade = quantidade;
-		this.preço = preço;
+		this.preco = preco;
+	}
+	
+	public Double getSubTotal() {
+		return (preco - desconto) * quantidade;
 	}
 
 	@JsonIgnore
@@ -67,11 +71,11 @@ public class ItemPedido implements Serializable {
 	}
 
 	public Double getPreço() {
-		return preço;
+		return preco;
 	}
 
-	public void setPreço(Double preço) {
-		this.preço = preço;
+	public void setPreço(Double preco) {
+		this.preco = preco;
 	}
 
 	@Override
